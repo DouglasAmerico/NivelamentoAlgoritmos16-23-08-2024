5c1030493532e947e26f62ddab9da7510e49c96b
Douglas Américo <douglas@likesistemas.com.br>
2024-08-16 21:49:28 -0300

    Inserindo o calculo de horas-minutos-segundos com base nos segundos

--- calculoDeSegundosEmHorasMinutosSegundos.java
+++ calculoDeSegundosEmHorasMinutosSegundos.java
@@ -4,8 +4,11 @@ public class calculoDeSegundosEmHorasMinutosSegundos {
     public static void main(String[] args) {
         Scanner prompt = new Scanner(System.in);
         System.out.print("Informe os segundos: ");
-        int segundos;
-
-
+        int segundos = prompt.nextInt();
+        int horas = segundos/60/60;
+        segundos= segundos - (horas*60*60);
+        int minutos= segundos/60;
+        segundos= segundos - (minutos*60);
+        System.out.println("Horas: "+horas+" Minutos: "+minutos+" Segundos: "+segundos);
     }
 }
