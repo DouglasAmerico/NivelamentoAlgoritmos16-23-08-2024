4e8f527a1ba06cfab7cf06b950b2ca639eb926ea
Douglas Américo <douglas@likesistemas.com.br>
2024-08-16 21:32:28 -0300

    Inserindo o .gitignore

---
+++ calculoDeSegundosEmHorasMinutosSegundos.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class calculoDeSegundosEmHorasMinutosSegundos {
+    public static void main(String[] args) {
+        Scanner prompt = new Scanner(System.in);
+        System.out.print("Informe os segundos: ");
+        int segundos;
+
+
+    }
+}
