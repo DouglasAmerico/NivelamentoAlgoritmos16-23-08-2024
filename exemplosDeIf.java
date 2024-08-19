68bc21ed6316b10ecbdedf258cda2f8deea95391
unknown <Administrador@gi02tads18.integrado.int>
2024-08-19 20:55:07 -0300

    Testando o Note da Faculdade

--- exemplosDeIf.java
+++ exemplosDeIf.java
@@ -2,7 +2,7 @@ public class exemplosDeIf {
     public static void main(String[] args) {
         if(2 <= 3 && 3 == 4){
             System.out.println("Certo");
-        }else{
+        }else if (1 == 1){
             System.out.println("Errado");
         }
     }
